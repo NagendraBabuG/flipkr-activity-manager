a6a40ff1dbdca97fd466057e3a8324406510dbf8
Nagendra Babu G <gnagendra0009@gmail.com>
2022-10-08 20:56:15 +0530

    Modified employer model schema

--- models/employee.js
+++ models/employee.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
+var objectId = Schema.objectId
 const employeeSchema = new Schema({
     name: {
         type: String, required: true
@@ -22,6 +23,9 @@ const employeeSchema = new Schema({
         type: Boolean, default: false
     }
     ,
+    adminId: {
+        type: mongoose.Schema.Types.ObjectId, default: null
+    },
     // employerId: {
     //     type: String
     // },
